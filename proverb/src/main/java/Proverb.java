c22a91bd30178b82605b430c6138f41b3ad23d84
Iain Diamond <iain.diamond@gmail.com>
2019-01-03 14:43:56 +0000

    inline variables

--- proverb/src/main/java/Proverb.java
+++ proverb/src/main/java/Proverb.java
@@ -1,26 +1,23 @@
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 class Proverb {
-  private List<String> causes;
-  private String lastCause;
   private String proverb = "";
 
   Proverb(String[] words) {
     if (words.length > 0) {
-      this.causes = Arrays.stream(words).skip(1).collect(Collectors.toList());
-      this.lastCause = words[0];
+      var causes = Arrays.stream(words).skip(1).collect(Collectors.toList());
+      var lastCause = words[0];
 
       var builder = new StringBuilder();
-      var previous = this.lastCause;
+      var previous = lastCause;
 
       for (String cause : causes) {
         builder.append(String.format("For want of a %s the %s was lost.\n", previous, cause));
         previous = cause;
       }
 
-      builder.append(String.format("And all for the want of a %s.", this.lastCause));
+      builder.append(String.format("And all for the want of a %s.", lastCause));
       proverb = builder.toString();
     }
   }
