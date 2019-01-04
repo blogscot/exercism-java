d6fb894deff5ec7209006060eb9f56626f9189ec
Iain Diamond <iain.diamond@gmail.com>
2019-01-04 12:53:20 +0000

    Filter digits using replaceAll

--- isbn-verifier/src/main/java/IsbnVerifier.java
+++ isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,27 +1,16 @@
-import java.util.List;
-import java.util.stream.Collectors;
-
 class IsbnVerifier {
 
   boolean isValid(String stringToVerify) {
     if (stringToVerify.isEmpty()) return false;
 
-    List<Integer> digits = stringToVerify
-        .codePoints()
-        .mapToObj(i -> (char) i)
-        .filter(Character::isDigit)
-        .map(Character::getNumericValue)
-        .collect(Collectors.toList());
+    var digits = stringToVerify.replaceAll("([^\\dX])", "").toCharArray();
 
-    var containsCheckDigit = stringToVerify.charAt(stringToVerify.length() - 1) == 'X';
-    var digitsSize = digits.size();
+    if (digits.length != 10) return false;
 
-    if (!(digitsSize == 10 && !containsCheckDigit ||
-        digitsSize == 9 && containsCheckDigit)) return false;
-
-    var total = containsCheckDigit ? 10 : 0;
-    for (int i = 0; i < digitsSize; i++) {
-      total += (10 - i) * digits.get(i);
+    var total = 0;
+    for (int i = 0; i < digits.length; i++) {
+      var digit = digits[i];
+      total += (digit == 'X' && i == 9) ? 10 : (10 - i) * Character.getNumericValue(digit);
     }
 
     return total % 11 == 0;
